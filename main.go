d1b17d4cf25429d3b1d8ec184ad694dc78731f5b
devs.shakil <devs.shakil@gmail.com>
2025-08-18 21:47:06 +0600

    first commit

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+import "fmt"
+
+// function
+
+// func main(){
+// 	fmt.Println("Hello, World!")
+// 	fmt.Println("This is a simple Go program.")
+// 	fmt.Println("Let's learn Go together!")
+// }
+
+// variables
+
+// func main() {
+// 	// int, string, float32, bool
+// 	var a int = 10
+// 	var b int = 20
+// 	var c int = a + b
+// 	fmt.Println("The sum of", a, "and", b, "is", c);
+
+// 	var name string = "shakil"
+// 	var wife string = "Tanzina"
+// 	fmt.Println("My name is", name, "and my wife's name is", wife);
+
+// 	var areyoumarried  = true;
+// 	fmt.Println("Are you married?", areyoumarried);
+// }
+
+
+// if else statement
+// func main() {
+// 	var age int = 10;
+// 	if(age < 18 && age >= 14){
+// 		fmt.Println("You are not eligible to marriage")
+// 	}else if(age >= 18 && age < 30){
+// 		fmt.Println("You are eligible to marriage but you should wait for few years");
+// 	}else if (age < 14 ){
+// 		fmt.Println("You are a baby")
+// 	}else{
+// 		fmt.Println("You are eligible to marriage")
+// 	}
+// }
+
+// function
+// func sum(a int, b int)int {
+// 	return a + b
+// }
+// func main() {
+// 	result := sum(10, 20)
+// 	fmt.Println("The sum is:", result)
+// }
+
+func mulsum(a int, b int)(int, int){
+	mul := a * b;
+	sum := a + b;
+	return mul, sum
+}
+
+func main(){
+	a := 100;
+	b := 200;
+	mul, sum := mulsum(a, b);
+	fmt.Println("The multiplication is:", mul);
+	fmt.Println("The sum is:", sum);
+}
